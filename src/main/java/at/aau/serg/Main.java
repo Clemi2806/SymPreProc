69472cc48d53853c1fe63b917eb4fad7d86561f5
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2024-07-22 11:04:54 +0200

    Add simple CLI functionality

--- src/main/java/at/aau/serg/Main.java
+++ src/main/java/at/aau/serg/Main.java
@@ -1,9 +1,37 @@
 package at.aau.serg;
 
-import org.apache.commons.cli.Options;
+import at.aau.serg.cli.CliUtils;
+import org.apache.commons.cli.*;
 
 public class Main {
     public static void main(String[] args) {
-        Options options = new Options();
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(CliUtils.getOptions(), args);
+        } catch (ParseException e) {
+            CliUtils.printHelp();
+            System.exit(1);
+        }
+
+        String sourceFile = cmd.getOptionValue("source");
+        String outputFile = cmd.getOptionValue("output");
+        String classFile = cmd.getOptionValue("class");
+        String methodName = cmd.getOptionValue("method");
+
+        if(sourceFile == null) {
+            System.err.println("Source file required");
+        }
+        if(outputFile == null) {
+            System.err.println("Output file required");
+        }
+        if(classFile == null) {
+            System.err.println("Class file required");
+        }
+        if(methodName == null) {
+            System.err.println("Method name required");
+        }
+
+
     }
 }
