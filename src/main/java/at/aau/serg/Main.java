71a15779265c549408d1738137629b397db96da7
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2024-07-22 10:40:46 +0200

    Initial commit

---
+++ src/main/java/at/aau/serg/Main.java
@@ -0,0 +1,9 @@
+package at.aau.serg;
+
+import org.apache.commons.cli.Options;
+
+public class Main {
+    public static void main(String[] args) {
+        Options options = new Options();
+    }
+}
