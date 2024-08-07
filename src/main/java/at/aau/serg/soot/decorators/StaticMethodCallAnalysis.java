aaacf5c37f7aba06436fe7f492693a4b19ea0562
Clemens Bauer <38780028+Clemi2806@users.noreply.github.com>
2024-08-07 09:13:46 +0200

    Added predicate to filter out method calls with higher precision

--- src/main/java/at/aau/serg/soot/decorators/StaticMethodCallAnalysis.java
+++ src/main/java/at/aau/serg/soot/decorators/StaticMethodCallAnalysis.java
@@ -7,14 +7,14 @@ import sootup.core.signatures.MethodSignature;
 import sootup.core.types.PrimitiveType;
 import sootup.java.core.JavaSootMethod;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StaticMethodCallAnalysis extends AnalysisDecorator{
+    private static final List<String> EXCLUDED_SCOPES = Arrays.asList("java.lang", "java.util");
+
     public StaticMethodCallAnalysis(Analysis analysis) {
         super(analysis);
     }
@@ -27,8 +27,10 @@ public class StaticMethodCallAnalysis extends AnalysisDecorator{
     }
 
     private Set<StaticMethodCall> getStaticMethodCalls() {
-        Predicate<JavaSootMethod> isStatic = method -> method.isStatic() && !method.getName().startsWith("<") && !method.isBuiltInMethod();
-        Predicate<JavaSootMethod> isOfIntType = method -> PrimitiveType.isIntLikeType(method.getReturnType());
+        Predicate<JavaSootMethod> isStatic = method -> method.isStatic() && !method.getName().startsWith("<");
+        Predicate<JavaSootMethod> isUsableType = method -> PrimitiveType.isIntLikeType(method.getReturnType()) || method.getReturnType() instanceof PrimitiveType.DoubleType || method.getReturnType() instanceof PrimitiveType.FloatType;
+        Predicate<JavaSootMethod> isNotMethodOfExcludedScope = method ->  EXCLUDED_SCOPES.stream().noneMatch(s -> method.getDeclaringClassType().getPackageName().toString().startsWith(s));
+        Predicate<JavaSootMethod> isNotMethodOfSameClass = method -> !method.getDeclaringClassType().equals(getMethod().getDeclaringClassType());
 
         Function<MethodSignature, JavaSootMethod> getSootMethodUsingSignature = signature -> getView().getMethod(signature).orElseThrow(() ->  new RuntimeException("Method " + signature.getName() + " not found"));
         Function<JavaSootMethod, StaticMethodCall> convertToStaticMethodCall = method -> new StaticMethodCall(method.getDeclaringClassType().getClassName(), method.getName(), (PrimitiveType) method.getReturnType());
@@ -36,7 +38,9 @@ public class StaticMethodCallAnalysis extends AnalysisDecorator{
         return getCallGraph().callsFrom(getMethod().getSignature()).stream()
                 .map(getSootMethodUsingSignature)
                 .filter(isStatic)
-                .filter(isOfIntType)
+                .filter(isUsableType)
+                .filter(isNotMethodOfExcludedScope)
+                .filter(isNotMethodOfSameClass)
                 .map(convertToStaticMethodCall)
                 .collect(Collectors.toSet());
     }
